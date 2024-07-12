7842074e316daca778a8d23aa38dd425ed68603d
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-12 11:04:54 +0800

    add js file, set proper id, set var for js

---
+++ task.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(event){
+
+    var taskInput = document.getElementById("task-input");
+    var inputButton = document.getElementById("input-button");
+    var taskHolder = document.getElementById("task-holder");
+    
+});
