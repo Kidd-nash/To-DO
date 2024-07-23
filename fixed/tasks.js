9a47d8de216d5a56a8ef2b08065d5c5db707fed1
Kidd-nash <arkady.pantaleon@outlook.com>
2024-07-23 13:52:51 +0800

    added js file with initial code flow

---
+++ fixed/tasks.js
@@ -0,0 +1,26 @@
+window.addEventListener( load, function (event) {
+  console.log('start')
+    //declare variables from html
+
+    //Display tasks
+
+    //Pre made tasks
+
+        //fetch tasks from api
+
+    //New tasks
+
+        //Validate Input (task name, date)
+
+    //3 main processes for both Pre made and New tasks
+
+        //add
+
+        //edit
+
+        //delete
+
+
+
+
+});
